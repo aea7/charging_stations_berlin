0aa82b0fb67a55486b1269e60ea5192724d62482
Abdullah Ak <aeak1994@gmail.com>
2021-03-28 14:43:00 +0200

    first messy commit that works

---
+++ src/components/Maps/index.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import axios from "axios";
+
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+class Mapz extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Deed Challenge",
+      locations: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.openchargemap.io/v3/poi/?output=json&countrycode=DE&maxresults=10&compact=true&verbose=false&latitude=52.520008&longitude=13.404954&distance=10&distanceunit=KM&key=14c14de6-4c0a-4d11-9498-93259fca2dd9`
+      )
+      .then((res) => {
+        const locations = res.data;
+        this.setState({ locations });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={13}
+          style={mapStyles}
+          initialCenter={{
+            lat: 52.5188455657791,
+            lng: 13.4039381981476,
+          }}
+        >
+          {this.state.locations.map((location, index) => (
+            <Marker
+              animation= {this.props.google.maps.Animation.DROP}
+              key={index}
+              position={
+                {lat: location.AddressInfo.Latitude, lng: location.AddressInfo.Longitude}
+              }
+              // onClick={alert(location.AddressInfo.Title)}
+            />
+          ))}
+        </Map>
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyD6T8zNeCCXnR0NhbSplOFvaHG6Jfa6X70",
+})(Mapz);
