beaef9e9ff8d36b2f6a784c6cbd6dbf205ffc75b
Abdullah Ak <aeak1994@gmail.com>
2021-03-28 14:47:48 +0200

    added a loader p element

--- src/components/Maps/index.jsx
+++ src/components/Maps/index.jsx
@@ -11,45 +11,51 @@ class Mapz extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Deed Challenge",
+      loading: false,
       locations: [],
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     axios
       .get(
         `https://api.openchargemap.io/v3/poi/?output=json&countrycode=DE&maxresults=10&compact=true&verbose=false&latitude=52.520008&longitude=13.404954&distance=10&distanceunit=KM&key=14c14de6-4c0a-4d11-9498-93259fca2dd9`
       )
       .then((res) => {
         const locations = res.data;
-        this.setState({ locations });
+        this.setState({ locations: locations, loading: false });
       });
   }
 
   render() {
     return (
       <div>
-        <Map
-          google={this.props.google}
-          zoom={13}
-          style={mapStyles}
-          initialCenter={{
-            lat: 52.5188455657791,
-            lng: 13.4039381981476,
-          }}
-        >
-          {this.state.locations.map((location, index) => (
-            <Marker
-              animation= {this.props.google.maps.Animation.DROP}
-              key={index}
-              position={
-                {lat: location.AddressInfo.Latitude, lng: location.AddressInfo.Longitude}
-              }
-              // onClick={alert(location.AddressInfo.Title)}
-            />
-          ))}
-        </Map>
+        {this.state.loading ? (
+          <p>Be Hold, fetching data may take some time :)</p>
+        ) : (
+          <Map
+            google={this.props.google}
+            zoom={13}
+            style={mapStyles}
+            initialCenter={{
+              lat: 52.5188455657791,
+              lng: 13.4039381981476,
+            }}
+          >
+            {this.state.locations.map((location, index) => (
+              <Marker
+                animation={this.props.google.maps.Animation.DROP}
+                key={index}
+                position={{
+                  lat: location.AddressInfo.Latitude,
+                  lng: location.AddressInfo.Longitude,
+                }}
+                // onClick={alert(location.AddressInfo.Title)}
+              />
+            ))}
+          </Map>
+        )}
       </div>
     );
   }
